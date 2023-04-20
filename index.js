8725d8652242e2c8bc8f5d2cd6cb0ee09021bf78
Carolina <carolina.peletay@gmail.com>
2023-04-20 04:20:54 -0300

    My first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.nav-link').hover(
+      function () {
+        $(this).css('font-size', '18px');
+      },
+      function () {
+        $(this).css('font-size', '16px');
+      }
+    );
+  });
+  
